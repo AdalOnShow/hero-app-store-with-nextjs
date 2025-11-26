6188c7fb09550bd521ab73c90690a1930836b883
Sharif H. Adal <mdadalbadsha2008@gmail.com>
2025-11-26 17:31:51 +0600

    fix erro in login page

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -5,15 +5,14 @@ import { toast } from "sonner";
 import useAuth from "@/hooks/useAuth";
 import PrimaryBtn from "@/components/PrimaryBtn";
 import Link from "next/link";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 const Login = () => {
   const { loginUserFunc, setUser, googleSigninFunc } = useAuth();
   const [loginLoader, setLoginLoader] = useState(false);
 
   const router = useRouter();
-  const searchParams = useSearchParams();
-  const from = searchParams.get("from") || "/";
+  const from = "/"; // fixed redirect path
 
   const handleSigninError = (error) => {
     switch (error.code) {
@@ -44,39 +43,36 @@ const Login = () => {
     const password = e.target.password.value;
 
     loginUserFunc(email, password)
-      .then((result) => {
-        const user = result.user;
-        setUser(user);
+      .then((res) => {
+        setUser(res.user);
         toast.success("Login successful!");
 
         setTimeout(() => {
           router.replace(from);
         }, 300);
       })
-      .catch((error) => {
-        toast.error(handleSigninError(error));
+      .catch((err) => {
+        toast.error(handleSigninError(err));
         setLoginLoader(false);
       });
   };
 
   const handleGoogleSignin = () => {
     googleSigninFunc()
-      .then((result) => {
-        setUser(result.user);
+      .then((res) => {
+        setUser(res.user);
         toast.success("Login successful!");
 
         setTimeout(() => {
           router.replace(from);
         }, 300);
       })
-      .catch((error) => {
-        toast.error(handleSigninError(error));
-      });
+      .catch((err) => toast.error(handleSigninError(err)));
   };
 
   return (
     <div className="max-w-11/12 mx-auto">
-      <div className="flex flex-col items-center justify-center h-[80dvh] dark">
+      <div className="flex flex-col items-center justify-center h-[80vh] dark">
         <div className="w-full max-w-md bg-gray-800 rounded-lg shadow-md p-6">
           <h2 className="text-2xl font-bold text-gray-200 mb-4 text-center">
             Login Model Stack
@@ -90,6 +86,7 @@ const Login = () => {
               name="email"
               required
             />
+
             <input
               placeholder="Password"
               className="form-input"
@@ -115,8 +112,10 @@ const Login = () => {
 
           <div className="divider divider-info text-white">OR</div>
 
-          <button onClick={handleGoogleSignin} className="btn bg-white text-black border-[#e5e5e5]">
-            <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
+          <button
+            onClick={handleGoogleSignin}
+            className="btn bg-white text-black border-[#e5e5e5]"
+          >
             Login with Google
           </button>
         </div>
